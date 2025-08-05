8ed220de44fe62cbfea4fd90fd9c78137f9803d7
sena <sena@ESPDFPCAJGOD227.SENA.RED>
2025-08-05 15:31:16 -0500

    Ajustes en el frontend

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define:{
-    'import.meta.env': import.meta.env
-  }
+  server: {
+    port: 5173,
+    host: true,
+  },
 })
